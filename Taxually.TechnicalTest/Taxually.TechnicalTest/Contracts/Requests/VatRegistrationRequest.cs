fed95cb70e8f80e977528f87d397e77429e713ca
LeventeFabry <levente.fabry@matterofform.com>
2025-02-18 13:24:00 +0100

    refactor - add API validation

--- Taxually.TechnicalTest/Taxually.TechnicalTest/Contracts/Requests/VatRegistrationRequest.cs
+++ Taxually.TechnicalTest/Taxually.TechnicalTest/Contracts/Requests/VatRegistrationRequest.cs
@@ -1,10 +1,19 @@
-﻿namespace Taxually.TechnicalTest.Contracts.Requests;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Taxually.TechnicalTest.Contracts.Requests;
 
 public readonly record struct VatRegistrationRequest
 {
+    [Required]
+    [MaxLength(250)]
     public string CompanyName { get; init; }
     
+    [Required]
+    [MaxLength(250)]
     public string CompanyId { get; init; }
     
+    [Required]
+    // I used this solution for the simplicity but in a real-world scenario I would use FluentValidation
+    [RegularExpression("uk|fr|de|UK|FR|DE", ErrorMessage = "Invalid Country Code")]
     public string Country { get; init; }
 }
